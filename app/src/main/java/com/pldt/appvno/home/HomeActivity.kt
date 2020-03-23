7f4b307df1b8f0d09f607d0ed3636342e1975a61
Arjay Lipio <arjay.lipio@mdscsi.com>
2020-03-24 05:59:45 +0800

    Change Framelayout to Viewpager

--- app/src/main/java/com/pldt/appvno/home/HomeActivity.kt
+++ app/src/main/java/com/pldt/appvno/home/HomeActivity.kt
@@ -2,13 +2,19 @@ package com.pldt.appvno.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.pldt.appvno.R
 import com.pldt.appvno.extensions.isVisible
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+
+    var prevMenuItem: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,31 +41,60 @@ class HomeActivity : AppCompatActivity() {
             // Open Activity For calling
         }
 
+
+        container_fragment_home.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                if (prevMenuItem != null) {
+                    prevMenuItem?.isChecked = false
+                } else {
+                    navigation_bottom_home.menu.getItem(0).isChecked = false
+                }
+                Log.d("page", "onPageSelected: $position")
+                navigation_bottom_home.menu.getItem(position).isChecked = true
+                prevMenuItem = navigation_bottom_home.menu.getItem(position)
+
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+
         navigation_bottom_home.setOnNavigationItemSelectedListener {
             when(it.itemId){
                 R.id.menu_home_bottomNavigation -> {
-                    replaceFragment(HomeFragment.newInstance())
+                    container_fragment_home.currentItem = it.itemId
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.menu_messages_bottomNavigation -> {
-                    replaceFragment(MessageFragment.newInstance())
+                    container_fragment_home.currentItem = it.itemId
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.menu_call_bottomNavigation -> {
-                    replaceFragment(CallFragment.newInstance())
+                    container_fragment_home.currentItem = it.itemId
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.menu_shop_bottomNavigation -> {
-                    replaceFragment(ShopFragment.newInstance())
+                    container_fragment_home.currentItem = it.itemId
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.menu_others_bottomNavigation -> {
-                    replaceFragment(OtherFragment.newInstance())
+                    container_fragment_home.currentItem = it.itemId
                     return@setOnNavigationItemSelectedListener true
                 }
             }
             return@setOnNavigationItemSelectedListener false
         }
+
+        setupViewPager()
     }
 
     fun replaceFragment(fragment: Fragment){
@@ -69,4 +104,15 @@ class HomeActivity : AppCompatActivity() {
             commit()
         }
     }
+
+    private fun setupViewPager() {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomeFragment.newInstance())
+        adapter.addFragment(MessageFragment.newInstance())
+        adapter.addFragment(CallFragment.newInstance())
+        adapter.addFragment(ShopFragment.newInstance())
+        adapter.addFragment(OtherFragment.newInstance())
+        container_fragment_home.adapter = adapter
+    }
+
 }
